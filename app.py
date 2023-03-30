cef87e1a0fd0f3a8e0016c2249ef267f4eb6ed9d
Frank <chenjiayi_344@hotmail.com>
2023-03-30 17:29:34 +0200

    "Initialize and generate a dictionary of page names and their respective functions, create a dropdown menu in the sidebar for the user to select the desired page to display, and call the selected function to display the chosen page."

--- app.py
+++ app.py
@@ -7,15 +7,40 @@ programming interface (API). The user selects their desired page \
 from the sidebar dropdown menu.
 """
 
+import sys
+import traceback
 import streamlit as st
+from optimizer.core.initialisation import initialise
 from optimizer.core.introduction import intro
-from optimizer.tests.tests import testAPI
+from optimizer.core.job import job_description
+from optimizer.tests.tests import test_api
 
 
-page_names_to_funcs = {
-    "Introduction": intro,
-    "Test API": testAPI,
-}
+def main():
+    """
+    Runs the main process of the application. The function initializes and generates a dictionary containing the desired page names and their respective functions. This dictionary is then used to create a dropdown menu in the sidebar, allowing the user to select the desired page to display. The selected function is then called to display the chosen page.
 
-demo_name = st.sidebar.selectbox("Choose a page", page_names_to_funcs.keys())
-page_names_to_funcs[demo_name]()
+    Returns:
+    None
+    """
+
+    initialise()
+    page_names_to_funcs = {
+        "Introduction": intro,
+        "Test API": test_api,
+        "Job description": job_description,
+    }
+
+    demo_name = st.sidebar.selectbox(
+        "Choose a page", page_names_to_funcs.keys())
+    page_names_to_funcs[demo_name]()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+    except Exception as error:
+        trace_back_obj = sys.exc_info()
+        traceback.print_tb(trace_back_obj)
