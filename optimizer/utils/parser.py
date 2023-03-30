2eafb6043ca0117b56b8c056c3f04846a8fe3eea
Frank <chenjiayi_344@hotmail.com>
2023-03-31 01:30:35 +0200

    Add functions to query project title, description, and contributions using OpenAI's GPT3 model

--- optimizer/utils/parser.py
+++ optimizer/utils/parser.py
@@ -13,6 +13,10 @@ from optimizer.gpt.api import MODEL, call_openai_api
 from optimizer.utils.extract import extract_code, extract_by_quotation_mark
 
 
+SECRETARY_ROLE = """You are my secretary. I need you to identify and \
+extract all the information of a resume. You have to do it very carefully."""
+
+
 def search_field(obj: dict, candidates: list) -> str | list | dict:
     """
     Search for the first matching field in the given object, selected from a list of candidates.
@@ -116,3 +120,101 @@ def get_date_range(exp: dict) -> str:
     str_range = f"{month_start} {exp['start']['year']} - \
                   {month_end} {exp['end']['year']}"
     return str_range
+
+
+@st.cache_data
+def query_project_title(project_info: str) -> str:
+    """
+    This function sends a list of messages to an OpenAI API for processing \
+    and returns the extracted project name from the API response.
+
+    Args:
+    project_info (str): The information about the project that needs to \
+    be queried.
+
+    Returns:
+    str: The extracted project name surrounded with code tags.
+    """
+    messages = [
+        {"role": "system", "content": SECRETARY_ROLE},
+        {"role": "user", "content": "The following is the resume"},
+        {"role": "user", "content": st.session_state['txt_resume']},
+        {"role": "user", "content": f"Can you find the project name with \
+        this infomration: {project_info}?"},
+        {"role": "user", "content": "Please always surround the output \
+        with code tags by using the following syntax:"},
+        {"role": "user", "content": "<code> Your message here </code>"},
+    ]
+    reply = call_openai_api(MODEL, messages, temperature=0.1)
+    result_str = extract_code(reply)
+    if result_str is None:
+        result_str = extract_by_quotation_mark(reply)
+    return result_str
+
+
+@st.cache_data
+def query_project_description(project_name: str) -> str:
+    """
+    Returns the project description of a given `project_name`.
+
+    Args:
+    project_name (str): The name of the project to extract the description from.
+
+    Returns:
+    str: The extracted project description, surrounded by code tags.
+    """
+    messages = [
+        {"role": "system", "content": SECRETARY_ROLE},
+        {"role": "user", "content": "The following is the resume"},
+        {"role": "user", "content": st.session_state['txt_resume']},
+        {"role": "user", "content": f"Can you extract the project \
+        description of Project:  {project_name}, between project name and \
+        key contributions?"},
+        {"role": "user", "content": "Please always surround the output \
+        with code tags by using the following syntax:"},
+        {"role": "user", "content": "<code> Your message here </code>"},
+    ]
+    reply = call_openai_api(MODEL, messages, temperature=0.1)
+    result_str = extract_code(reply)
+    return result_str
+
+
+@st.cache_data
+def query_project_contributions(project_name: str) -> list | None:
+    """
+    Takes in a project_name and generates a list of key contributions
+    of the project using OpenAI's GPT3 model.
+
+    Parameters:
+    -----------
+    project_name : str
+        The name of the project whose contributions need to be extracted.
+
+    Returns:
+    --------
+    contributions : list of str
+        The list of key contributions of the project. Each string in the
+        list has been stripped of non-alphanumeric characters and whitespaces.
+    """
+    messages = [
+        {"role": "system", "content": SECRETARY_ROLE},
+        {"role": "user", "content": "The following is my resume"},
+        {"role": "user", "content": st.session_state['txt_resume']},
+        {"role": "user", "content": f"Can you extract the key \
+        contributions of Project:  {project_name}?"},
+        {"role": "user", "content": "Please always surround the output \
+        with code tags by using the following syntax:"},
+        {"role": "user", "content": "<code> Your message here </code>"},
+    ]
+    reply = call_openai_api(MODEL, messages, temperature=0.1)
+    result_str = extract_code(reply)
+
+    # assemble contributions list, which contains strings that have \
+    # been stripped of non-alphanumeric characters and whitespace.
+    if result_str is not None:
+        contributions = result_str.strip().split('\n')
+        contributions = [re.sub(r'[^A-Za-z0-9 ]+', '', c)
+                         for c in contributions]
+    else:
+        contributions = []
+    return contributions
