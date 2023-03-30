ff717f0400cce226e615e3521d840aa20b2b3987
Frank <chenjiayi_344@hotmail.com>
2023-03-30 17:02:32 +0200

    Add initialisation module for Streamlit session state and text fields

---
+++ optimizer/core/initialisation.py
@@ -0,0 +1,36 @@
+"""
+This module provides utility functions for initializing Streamlit \
+session state and text fields.
+"""
+
+import streamlit as st
+
+
+def init_state(name, value=False):
+    """
+    Initialise session_state with provide field name and value
+    """
+
+    if name not in st.session_state:
+        st.session_state[name] = value
+
+
+def initialise():
+    """
+    Initialises controllers states and text fields
+
+    Returns:
+    None
+    """
+    # initialise controllers states
+    button_states = ['btn_summary', 'btn_analyse', 'btn_generate_statement',
+                     'btn_sort_skills', 'btn_generate_skills', 'new_statements',
+                     'new_skills']
+    for button in button_states:
+        init_state(button)
+
+    # initialise text fields
+    text_fields = ['txt_jd']
+
+    for text in text_fields:
+        init_state(text, '')
