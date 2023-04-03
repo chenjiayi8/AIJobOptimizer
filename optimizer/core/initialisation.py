1d2a4216e0c9b9bb5fcce81f33b92424f8ad2337
Frank <chenjiayi_344@hotmail.com>
2023-04-03 23:55:15 +0200

    Add `reset()` function to initialise session state and text fields and reset the current session, while keeping the text input of resume

--- optimizer/core/initialisation.py
+++ optimizer/core/initialisation.py
@@ -4,7 +4,7 @@ session state and text fields.
 """
 
 import streamlit as st
-
+from streamlit_extras.switch_page_button import switch_page
 from optimizer.utils.format import custom_layout
 
 
@@ -25,14 +25,16 @@ def initialise():
     None
     """
     # initialise controllers states
-    button_states = ['btn_summary', 'btn_analyse', 'btn_generate_statement',
-                     'btn_sort_skills', 'btn_generate_skills', 'new_statements',
+    button_states = ['btn_summary', 'btn_analyse', 'btn_estimate',
+                     'btn_generate_statement', 'btn_sort_skills',
+                     'btn_generate_skills', 'new_statements',
                      'new_skills']
     for button in button_states:
         init_state(button)
 
     # initialise text fields
-    text_fields = ['txt_jd', 'txt_resume', 'txt_skills', 'statement', 'skills']
+    text_fields = ['txt_jd', 'txt_resume', 'txt_skills',
+                   'statement', 'skills', 'dl_link']
 
     for text in text_fields:
         init_state(text, '')
@@ -44,3 +46,14 @@ def initialise():
         init_state(field, [])
 
     custom_layout()
+
+
+def reset():
+    """
+    Resets the current session while keeping the text input of resume. It \
+    then executes ''initialise()'' and ''switch_page(''Job description'')''.
+    """
+    if st.button("Warning: Reset", help="You will lose all your progress!"):
+        st.session_state = {'txt_resume': st.session_state['txt_resume']}
+        initialise()
+        switch_page('Job description')
