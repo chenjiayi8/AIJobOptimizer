e23f2d050a389afb925ac79cf216934f7936fe7f
Frank <chenjiayi_344@hotmail.com>
2023-04-04 23:25:46 +0200

    "Parse user's skills from comma-separated string and update UI accordingly."

--- pages/4_Core_Competencies.py
+++ pages/4_Core_Competencies.py
@@ -21,6 +21,22 @@ st.set_page_config(
 initialise()
 
 
+def parse_skills(reply):
+    """
+    Extracts a list of skills from a string of comma-separated skills.
+
+        Args:
+        - reply (str): A string containing comma-separated skills.
+
+        Returns:
+        - skills (list): A list of strings, each representing a parsed skill.
+    """
+    skills_str = extract_code(reply)
+    skills = skills_str.split(',')
+    skills = [skill.strip() for skill in skills]
+    return skills
+
+
 @st.cache_data(show_spinner=False)
 def sort_skills(txt_jd: str, skills: str) -> str:
     """
@@ -36,17 +52,19 @@ def sort_skills(txt_jd: str, skills: str) -> str:
     - reply (str): A string containing the sorted user's skills based on \
     their relevance to the job description.
     """
+    skills_str = ','.join(skills)
     messages = [
         {"role": "system", "content": SYSTEM_ROLE},
         {"role": "assistant", "content":  "The job description is following:"},
         {"role": "assistant", "content":  txt_jd},
         {"role": "user", "content": "I will give you my skills as following:"},
-        {"role": "user", "content": skills},
+        {"role": "user", "content": skills_str},
         {"role": "user", "content": "Please rank my skills in order of \
         relevance, based on the job description, starting with the most \
         in-demand skill to the least required."},
-        {"role": "user", "content": "Remove the duplicated, chain the result \
-        with ' | '"},
+        {"role": "user", "content": "Please remove the duplicated skills"},
+        {"role": "user", "content": "Please list the skills separated by \
+        commas: skill1, skill2, skill3"},
         {"role": "user", "content": "Please always surround the output with \
         code tags by using the following syntax:"},
         {"role": "user", "content": "<code> Your message here </code>"},
@@ -81,7 +99,9 @@ def generate_skills(number: int, words: int, temperature: float = 0.2) -> str:
         {"role": "user", "content": experiences_str},
         {"role": "user", "content": f"From my experiences, can you identify \
         and extract {number} skills which is demanded by the job desription? \
-        Each skill is less than {words} words. Chained the skills with ' | '"},
+        Each skill is less than {words} words."},
+        {"role": "user", "content": "Please list the skills separated by \
+        commas: skill1, skill2, skill3"},
         {"role": "user", "content": "Please always surround the output with \
         code tags by using the following syntax:"},
         {"role": "user", "content": "<code> Your message here </code>"},
@@ -133,8 +153,7 @@ def edit_skills():
             with st.spinner("Sorting"):
                 reply = sort_skills(
                     st.session_state['txt_jd'], st.session_state['skills'])
-                sorted_skills = extract_code(reply)
-                st.session_state['sorted_skills'] = sorted_skills
+                st.session_state['sorted_skills'] = parse_skills(reply)
 
     with col_skills_null:
         st.write("")
@@ -143,8 +162,7 @@ def edit_skills():
         if st.button('Generate skills', help="Generate skills from the job \
                     description and your experiences"):
             reply = generate_skills(skills_number, skills_words, skills_temp)
-            new_skills = extract_code(reply)
-            st.session_state['new_skills'] = ' | ' + new_skills.strip()
+            st.session_state['new_skills'] = parse_skills(reply)
             st.session_state['btn_generate_skills'] = True
             st.session_state['btn_sort_skills'] = False
 
