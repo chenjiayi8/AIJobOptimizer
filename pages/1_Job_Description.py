80688e08548348023804a2093c804b8fd8d52b5c
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-04-26 02:25:26 +0200

    Add URL validation and scraping for job description in job_description function

--- pages/1_Job_Description.py
+++ pages/1_Job_Description.py
@@ -5,6 +5,9 @@ by calling the OpenAI GPT API.
 import streamlit as st
 from optimizer.core.initialisation import initialise
 from optimizer.gpt.query import get_company_role, summary_job_description
+from optimizer.proxycurl.query import scrap_job_description
+from optimizer.utils.web import is_valid_url
+
 
 st.set_page_config(
     page_title="Job Description",
@@ -36,8 +39,20 @@ def job_description():
 
     if st.session_state['btn_summary']:
         with st.spinner("Summarying the job ..."):
+            if is_valid_url(st.session_state['txt_jd']):
+                scrapped_text = scrap_job_description(
+                    st.session_state['txt_jd'])
+                if scrapped_text is not None:
+                    st.session_state['txt_jd'] = scrapped_text
+                    st.experimental_rerun()
+                else:
+                    st.session_state['txt_jd'] = ""
+                    st.error("The URL is not valid. Please try again.")
+                    st.stop()
+
             job_analysed = summary_job_description(st.session_state['txt_jd'])
-            st.session_state['company_role'] = get_company_role()
+            if 'company_role' not in st.session_state:
+                st.session_state['company_role'] = get_company_role()
         st.markdown(f"__Position:__ {st.session_state['company_role'] }")
         st.markdown(f"__Summary:__ {job_analysed}")
 
