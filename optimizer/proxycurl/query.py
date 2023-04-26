06e8da7fa2cb7a75f3104f77f5311aafe6c25956
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-04-26 02:23:33 +0200

    Add function to query proxycurl API and extract LinkedIn job description

---
+++ optimizer/proxycurl/query.py
@@ -0,0 +1,27 @@
+"""
+This module consists of a set of functions to query proxycurl API to extract \
+the job description from a LinkedIn job posting.
+
+"""
+from optimizer.proxycurl.api import call_proxycurl_api
+from optimizer.utils.extract import exctract_linkedin_job_id
+from optimizer.utils.parser import parse_linkedin_job_description
+
+
+def scrap_job_description(url):
+    """
+    Scrapes the job description from a LinkedIn job posting.
+
+    Args:
+    url (str): The URL of the job posting.
+
+    Returns:
+    str: The job description.
+    """
+    job_id = exctract_linkedin_job_id(url)
+    if job_id is None:
+        return None
+    page = call_proxycurl_api(job_id)
+    scrapped_text = parse_linkedin_job_description(page)
+    scrapped_text = scrapped_text.replace('\\n', '\n')
+    return scrapped_text
