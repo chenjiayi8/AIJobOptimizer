4849a4a0971bce4adea4bc076b731908eb520bfe
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-06-21 12:01:04 +0200

    Update GPT-3 models and token limits in optimizer/gpt/api.py

--- optimizer/gpt/api.py
+++ optimizer/gpt/api.py
@@ -12,9 +12,10 @@ import requests
 from optimizer.gpt.token import num_tokens_from_messages
 from optimizer.utils.web import retry
 
-MODELS = ["gpt-3.5-turbo", "gpt-4"]
-TOKEN_LIMITS = {"gpt-3.5-turbo": 4096,
-                "gpt-4": 8192}
+MODELS = {"gpt-3.5-turbo": 4096,
+          "gpt-3.5-turbo-16k": 16384,
+          "gpt-4": 8192,
+          "gpt-4-32k": 32768}
 
 SYSTEM_ROLE = "You are my Career Coach. You will help me revise my resume for a target job."
 
